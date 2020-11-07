ba2ef474faa566677504c156628b8be41959724e
Max Kratzok <50670927+MKratzok@users.noreply.github.com>
2020-11-07 16:38:33 -0500

    Added command line code to main.py

--- Code/main.py
+++ Code/main.py
@@ -1,4 +1,6 @@
 import robot
+import sys
+
 
 def run_auto(r): #todo
     while True:
@@ -7,6 +9,7 @@ def run_auto(r): #todo
             r.turn(180)
 
     #Go forward until camera detects tape then turn and return
+
 
 def run_manual(r):
     #if extra time, then use arrow keys/don't press enter
@@ -26,8 +29,15 @@ def run_manual(r):
 
 
 if __name__ == "__main__":
-    # get cmd line argument for auto or manual mode  todo
-    auto = False
+    if len(sys.argv) is 1:
+        auto = False
+    elif sys.argv[1] in ['--auto', '-a']:
+        auto = True
+    elif sys.argv[1] in ['--manual', '-m']:
+        auto = False
+    else:
+        print('usage: main.py [-a or --auto] or [-m --manual]')
+        quit()
 
     r = robot.Robot()
 
@@ -40,6 +50,5 @@ if __name__ == "__main__":
 '''todo list:
     1) add camera code - J
     2) run auto (depends on camera) 
-    3) get cmd line arg for auto mode - M
     4) figure out how much to "curl up" 
     5) figure out how much servo should move to stick/unstick'''
